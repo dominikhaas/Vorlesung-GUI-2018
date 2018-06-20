a824201362ec3cb99094a399e9a9dd539bf1b75a
Dominik Haas <dominik.haas@qaware.de>
2018-06-20 13:18:40 +0200

    Musterloesung für Übung 09 Testing

--- codebase/u09-testing/test/de/fhrosenheim/gui/u09/SearchViewTest.java
+++ codebase/u09-testing/test/de/fhrosenheim/gui/u09/SearchViewTest.java
@@ -1,6 +1,7 @@
 package de.fhrosenheim.gui.u09;
 
 import java.util.Arrays;
+import java.util.List;
 import javafx.scene.Parent;
 import static org.hamcrest.Matchers.is;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.loadui.testfx.GuiTest;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -31,5 +33,18 @@ public class SearchViewTest extends GuiTest {
     @Override
     protected Parent getRootNode() {
         return new SearchView(searchService, searchResultSink);
+    }
+
+    @Test
+    public void testSearchKatze() {
+        // initialisiere test mock und gebe dummy ergebnisse zurueck
+        List<String> results = Arrays.asList("katze1.jpg", "katze2.jpg");
+        when(searchService.search("katze")).thenReturn(results);
+        
+        // interagiere mit UI, fuelle Textfeld und clicke button
+        pageObject.search("katze");
+
+        // pruefe ob ergbnisse angekommen sind
+        verify(searchResultSink, times(1)).process(results);
     }
 }
